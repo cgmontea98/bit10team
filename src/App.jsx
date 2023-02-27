c1aeebe69dae926c00e02d0f306c934436d1ad28
cgmontea98 <cgmontealegre9804@gmail.com>
2023-02-27 16:47:18 -0500

    publicando

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <h1>CVJS Dev's</h1>
       <h2>Consultoria</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
