8b317734b8a774f20842fc87c1717a4fa0a39eeb
cgmontea98 <cgmontealegre9804@gmail.com>
2023-03-07 20:53:19 -0500

    gh

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:cgmontea98/cgmontea98.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:cgmontea98/bit10team.git main:gh-pages
