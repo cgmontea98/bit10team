bbb7a3894da603174ecdc33501189fd9f72bccdf
cgmontea98 <cgmontealegre9804@gmail.com>
2023-03-08 19:54:57 -0500

    diseño vario

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -7,7 +7,7 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 export const Carrito = ({ showCart, cartItems, onHide }) => {
   return (
     <>
-      <Button variant="dark" onClick={onHide}>
+      <Button variant="dark" onClick={onHide} className="visually-hidden">
         <i className="fa-solid fa-cart-shopping text-white"></i>
       </Button>
       <Offcanvas show={showCart} onHide={onHide} placement="end">
@@ -17,18 +17,33 @@ export const Carrito = ({ showCart, cartItems, onHide }) => {
         <Offcanvas.Body>
           {cartItems &&
             cartItems.map((item) => (
-              <Card style={{ width: "18rem" }} key={item.id}>
-                <Card.Img variant="top" src={item.image_url} />
+              <Card
+                className="card text-bg-light mb-1 m-2"
+                style={{ textAlign: "center" }}
+                key={item.id}
+              >
+                <div
+                  style={{ display: "flex", justifyContent: "center" }}
+                  className="col"
+                >
+                  <Card.Img
+                    className="text-center py-3"
+                    variant="top"
+                    style={{ width: "60px" }}
+                    src={item.image_url}
+                  />
+                </div>
                 <Card.Body>
-                  <Card.Title>{item.name}</Card.Title>
-                </Card.Body>
-
-                <Card.Body>
-                  <ButtonGroup aria-label="Basic example">
-                    <Button variant="warning m-2">Left</Button>
-                    <Button variant="success m-2">Middle</Button>
-                    <Button variant="danger m-2">Right</Button>
-                  </ButtonGroup>
+                  <Card.Title style={{ textAlign: "center" }}>
+                    {item.name}
+                  </Card.Title>
+                  <Card.Body>
+                    <ButtonGroup aria-label="Basic example">
+                      <Button variant="warning m-2">Left</Button>
+                      <Button variant="success m-2">Middle</Button>
+                      <Button variant="danger m-2">Right</Button>
+                    </ButtonGroup>
+                  </Card.Body>
                 </Card.Body>
               </Card>
             ))}
@@ -37,4 +52,3 @@ export const Carrito = ({ showCart, cartItems, onHide }) => {
     </>
   );
 };
-
